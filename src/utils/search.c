8b8e0dcd0f44afe63eb723ca06e0e5b2e17f0a33
ML <louism0495@gmail.com>
2025-05-12 19:04:25 +0200

    utils function to search specific var in env

---
+++ src/utils/search.c
@@ -0,0 +1,15 @@
+#include "minishell.h"
+
+t_env_lst	*search_env_var(t_env_lst *env, char *var)
+{
+	t_env_lst	*tmp;
+
+	tmp = env;
+	while (tmp)
+	{
+		if (ft_strncmp(tmp->name, var, ft_strlen(tmp->name)) == 0)
+			return (tmp);
+		tmp = tmp->next; 
+	}
+	return (NULL);
+}
