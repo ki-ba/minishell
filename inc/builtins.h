5da01dcd12d11b7bf5cf3e2c6cee1471f8b528c2
ML <louism0495@gmail.com>
2025-05-12 19:00:18 +0200

    to test further, change dir, change env var PWD and OLDPWD

--- inc/builtins.h
+++ inc/builtins.h
@@ -2,7 +2,7 @@
 # define BUILTINS_H
 
 int	ft_echo(char *str, t_bool option);
-int	ft_cd(char *path);
+int	ft_cd(char *new_path, t_env_lst *env);
 int	ft_pwd(void);
 int ft_export(char *name, char *word);
 int ft_unset(char *name);
