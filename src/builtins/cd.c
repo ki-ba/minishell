cdcc9a6517706430e19433995536bd1f5a87cc8d
ML <louism0495@gmail.com>
2025-05-20 17:44:46 +0200

    add test for cd && pwd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -7,6 +7,8 @@ static int	no_arg_cd(char **cmd, t_env_lst *env);
 static int	update_env(char *new_path, t_env_lst *env);
 static int	check_dir_access(char *new_path);
 
+//TODO: go bad to check if `cd ""` function properly after cmd is properly passed
+
 /** @brief if relative => check access from end to start */
 /** @param cmd[0] is the cmd (here cd) */
 /** @param cmd[1] is the new path */
@@ -19,7 +21,7 @@ int	ft_cd(char **cmd, t_env_lst *env)
 
 	if (cmd[2] != NULL)
 	{
-		perror("minishell: cd: to many arguments\n");
+		ft_putendl_fd("minishell: cd: to many arguments", 2);
 		return (ERR_ARGS);
 	}
 	if (cmd[1] == NULL)
