76f1065f5a8dc9e5bf3737a6bd3edbf4a767b6e4
ML <louism0495@gmail.com>
2025-09-08 12:29:54 +0200

    fix envlist_to_arr

--- src/env/envlist_utils.c
+++ src/env/envlist_utils.c
@@ -6,7 +6,7 @@
 /*   By: mlouis <mlouis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 14:08:24 by mlouis            #+#    #+#             */
-/*   Updated: 2025/09/08 11:56:35 by mlouis           ###   ########.fr       */
+/*   Updated: 2025/09/08 12:26:02 by mlouis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,11 +46,10 @@ char	**envlist_to_arr(t_env_lst *env_lst)
 	env = ft_calloc(envlist_len(env_lst) + 1, sizeof(char *));
 	if (!env)
 	{
-		ft_putstr_fd("error converting env to array\n", 2);
 		return (NULL);
 	}
 	i = 0;
-	while (env && env_lst->next)
+	while (env && env_lst)
 	{
 		if (env_lst->value)
 			env[i] = ft_concat(3, env_lst->name, "=", env_lst->value);
@@ -58,7 +57,6 @@ char	**envlist_to_arr(t_env_lst *env_lst)
 			env[i] = ft_concat(2, env_lst->name, "=");
 		if (!env[i])
 		{
-			ft_putstr_fd("error converting env to array\n", 2);
 			ft_free_arr(env);
 			return (NULL);
 		}
